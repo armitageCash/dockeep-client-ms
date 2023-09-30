f38c9f8eba89b2ce6d2fe788a41bf9aab1b6fa47
Andrew Castro <aacastro661989@gmail.com>
2023-09-30 09:35:23 -0500

    update yml to IAC

---
+++ cloudformation-up.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Nombre de la pila de CloudFormation
+stack_name="dockep-clients-ms-stack"
+
+# Nombre del archivo JSON de la plantilla
+template_file="cloudformation-definition.json"
+
+# Nombre del archivo JSON de parámetros (opcional)
+# parameters_file="mis-parametros.json"
+
+# Nombre del archivo JSON de tags (opcional)
+
+# Región de AWS donde deseas crear la pila
+region="us-east-1"
+
+# Comando para crear la pila de CloudFormation
+aws cloudformation create-stack \
+    --stack-name "$stack_name" \
+    --template-body "file://$template_file" \
+   #  --parameters "file://$parameters_file" \
+   #  --tags "file://$tags_file" \
+    --region "$region"
+
+# Verificar si la creación de la pila fue exitosa
+if [ $? -eq 0 ]; then
+    echo "Pila de CloudFormation creada con éxito."
+else
+    echo "Error al crear la pila de CloudFormation."
+fi
