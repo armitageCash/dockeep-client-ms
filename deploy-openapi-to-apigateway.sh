4d759bdc28d2ddfae33006719a5ccb60cbeadfb5
Andrew Castro <aacastro661989@gmail.com>
2023-10-14 09:37:14 -0500

    export api id

--- deploy-openapi-to-apigateway.sh
+++ deploy-openapi-to-apigateway.sh
@@ -3,17 +3,17 @@
 api_id=$(aws apigateway import-rest-api --body "fileb://main.yml" --query 'id' --output text)
 echo "API ID: $api_id"
 
-# Luego, puedes exportar el valor del api-id para que esté disponible para otros comandos
-echo "::set-env name=API_ID::$api_id"
+# Exporta el valor de api_id como variable de entorno
+export API_ID="$api_id"
 
 # Define la etapa y la variable que deseas configurar
 STAGE_NAME="develop" # Cambia "production" a "develop"
 VARIABLE_NAME="ECS_ELB_ENDPOINT"
 VARIABLE_VALUE="http://dockeep-clients-ms-elb-1163464849.us-east-2.elb.amazonaws.com/"
 
-# Configura la stageVariable en tu API Gateway utilizando la variable api_id
+# Configura la stageVariable en tu API Gateway utilizando la variable API_ID
 aws apigateway update-stage \
-  --rest-api-id "$api_id" \
+  --rest-api-id "$API_ID" \
   --stage-name "$STAGE_NAME" \
   --patch-operations "op=replace,path=/variables/$VARIABLE_NAME,value=$VARIABLE_VALUE"
 
