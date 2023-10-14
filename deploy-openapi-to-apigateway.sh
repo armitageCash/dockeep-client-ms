fdfd96ab2a0511420ff6deab8da44357f3765428
Andrew Castro <aacastro661989@gmail.com>
2023-10-14 11:47:21 -0500

    update sh

--- deploy-openapi-to-apigateway.sh
+++ deploy-openapi-to-apigateway.sh
@@ -11,18 +11,28 @@ STAGE_NAME="develop" # Cambia "production" a "develop"
 VARIABLE_NAME="ECS_ELB_ENDPOINT"
 VARIABLE_VALUE="http://dockeep-clients-ms-elb-1163464849.us-east-2.elb.amazonaws.com/"
 
+# Crear una nueva implementación (deployment)
+deployment_id=$(aws apigateway create-deployment \
+  --rest-api-id "$API_ID" \
+  --stage-name "develop" \
+  --stage-description 'Development Stage' \
+  --description 'First deployment to the dev stage' \
+  --query "id" --output text)
+
 # Verifica si la etapa ya existe
 if aws apigateway get-stage --rest-api-id "$API_ID" --stage-name "$STAGE_NAME" &> /dev/null; then
   # Si la etapa ya existe, actualiza la variable
   aws apigateway update-stage \
     --rest-api-id "$API_ID" \
     --stage-name "$STAGE_NAME" \
+    --deployment-id "$deployment_id" \
     --patch-operations "op=replace,path=/variables/$VARIABLE_NAME,value=$VARIABLE_VALUE"
 else
   # Si la etapa no existe, crea la etapa y establece la variable
   aws apigateway create-stage \
     --rest-api-id "$API_ID" \
     --stage-name "$STAGE_NAME" \
+    --deployment-id "$deployment_id" \
     --variables "$VARIABLE_NAME=$VARIABLE_VALUE"
 fi
 
