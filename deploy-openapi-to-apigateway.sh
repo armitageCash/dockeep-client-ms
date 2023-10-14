d490f7bd7429ae3f0c929482020e37cdc75de6d5
Andrew Castro <aacastro661989@gmail.com>
2023-10-14 10:33:33 -0500

    update script

--- deploy-openapi-to-apigateway.sh
+++ deploy-openapi-to-apigateway.sh
@@ -7,7 +7,7 @@ echo "API ID: $api_id"
 export API_ID="$api_id"
 
 # Define la etapa y la variable que deseas configurar
-STAGE_NAME="develop" # Cambia "production" a "develop"
+STAGE_NAME="default" # Cambia "production" a "develop"
 VARIABLE_NAME="ECS_ELB_ENDPOINT"
 VARIABLE_VALUE="http://dockeep-clients-ms-elb-1163464849.us-east-2.elb.amazonaws.com/"
 
@@ -15,6 +15,6 @@ VARIABLE_VALUE="http://dockeep-clients-ms-elb-1163464849.us-east-2.elb.amazonaws
 aws apigateway update-stage \
   --rest-api-id "$API_ID" \
   --stage-name "$STAGE_NAME" \
-  --patch-operations "op=replace,path=/variables/$VARIABLE_NAME,value=$VARIABLE_VALUE"
+  --patch-operations "op=add,path=/variables/$VARIABLE_NAME,value=$VARIABLE_VALUE"
 
 # Aquí puedes agregar cualquier otro paso que necesites en tu script
